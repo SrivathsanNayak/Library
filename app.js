865dcf0be7fb589c8ec765bb2b811ae628f9ea62
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-08-20 20:48:45 +0530

    Added Book constructor initial code segment

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+    function Book (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+
+        this.info = function() {
+            return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+        }
+    }
+
+    const theHobbit = new Book('The Hobbit', 'JRR Tolkien', 295, 'read');
+    console.log(theHobbit.info());
+*/
