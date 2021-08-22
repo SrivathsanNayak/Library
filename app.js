8a99ea47a27feddc1d20b701b8fd8abd44028b1b
SrivathsanNayak <ganeshsrivathsan@gmail.com>
2021-08-22 15:34:24 +0530

    Added function to loop through array elements

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
-    //Array to store new book objects
-    let myLibrary = [];
+//Array to store new book objects
+let myLibrary = [];
 
-    //Constructor function to create new book object
-    function Book (title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
+//Constructor function to create new book object
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 
-        /*
-        this.info = function() {
-            return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-        }
-        */
-        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
-    }
+//Function to add book object to library
+function addBookToLibrary() {
+    let title = prompt("Title of book?", "The Hobbit");
+    let author = prompt("Name of author of book?", "JRR Tolkien");
+    let pages = prompt("Number of pages?", 295);
+    let read = prompt("Read or not read?", "read");
+    const obj = new Book(title, author, pages, read);
+    myLibrary.push(obj);
+    printBookInLibrary();
+}
 
+function printBookInLibrary() {
+    console.table(myLibrary);
+}
 
-    function addBookToLibrary () {
-        let title = prompt("Title of book?", "The Hobbit");
-        let author = prompt("Name of author of book?", "JRR Tolkien");
-        let pages = prompt("Number of pages?", 295);
-        let read = prompt("Read or not read?", "read");
-        const obj = new Book(title, author, pages, read);
-        myLibrary.push(obj);
-        console.log(myLibrary);
-    }
-
-    /*
-    //Calling constructor function to create Book
-    const theHobbit = new Book('The Hobbit', 'JRR Tolkien', 295, 'read');
-    console.log(theHobbit.info());
-    */
-
-    addBookToLibrary();
-    addBookToLibrary();
-    addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
